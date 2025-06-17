db4979eb2841e5f8101431ffee77f25366f014e2
Rehab Kamal <wwwrehabkamal601@gmail.com>
2025-06-18 00:11:01 +0300

    v2  search and books finished

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,32 +1,24 @@
-
 import React from "react";
-import  Navbar from "../components/HomePage/Navbar";
-import Hero from "../components/HomePage/Hero";
-import Blog from "../components/HomePage/Blog";
-import Footer from "../components/HomePage/Footer";
-import Categories from "../components/HomePage/Categories"; // Fixed import name
-import Welcome from "../components/HomePage/Welcome"; // Assuming you have a Welcome component 
-import NewReleases from "../components/HomePage/NewRelease";
-import Estimonialssection from "../components/HomePage/Estimonialssection"; // Assuming you have a testimonials section component
+import Navbar from "../components/HomePage/Navbar.jsx";
+import Hero from "../components/HomePage/Hero.jsx";
+import Blog from "../components/HomePage/Blog.jsx";
+import Footer from "../components/HomePage/Footer.jsx";
+import Categories from "../components/HomePage/Categories.jsx";
+import Welcome from "../components/HomePage/Welcome.jsx";
+import NewReleases from "../components/HomePage/NewRelease.jsx";
+import Estimonialssection from "../components/HomePage/Estimonialssection.jsx";
 
-
- const Home = () => {
+const Home = () => {
   return (
     <>
       <Navbar />
-       <Hero />
-      <Categories /> 
-      <Welcome /> 
+      <Hero />
+      <Categories />
+      <Welcome />
       <NewReleases />
       <Blog />
       <Estimonialssection />
       <Footer />
-
-
-
-      
-       
-    
     </>
   );
 };
